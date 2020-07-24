e2b529d03831c17c27be56be098b2dfc1d642f21
movchko <lemberg.my.city@gmail.com>
2020-07-24 20:59:13 +0300

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+/* global $ */
+
+$(document).ready(function() {
+  $('.header__top__burger').click(function(event) {
+    $('.header__top__burger, .header__dark, .nav')
+      .toggleClass('active');
+  });
+});
+
+window.onload = function() {
+  document.body.classList.add('loaded_hiding');
+
+  window.setTimeout(function() {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+};
