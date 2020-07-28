c1cc91fd26b7b79e60636a27766c0382cb19c9da
movchko <lemberg.my.city@gmail.com>
2020-07-29 00:29:01 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,22 @@ window.onload = function() {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 };
+
+$('.shop__nav__item').on('click', function() {
+  $('.shop__nav__item').removeClass('active');
+  $(this).addClass('active');
+
+  if ($('.shop__nav__item--body').hasClass('active')) {
+    $('.shop__cards').removeClass('active');
+    $('.body').addClass('active');
+  } else if ($('.shop__nav__item--face').hasClass('active')) {
+    $('.shop__cards').removeClass('active');
+    $('.face').addClass('active');
+  } else if ($('.shop__nav__item--hair').hasClass('active')) {
+    $('.shop__cards').removeClass('active');
+    $('.hair').addClass('active');
+  } else if ($('.shop__nav__item--candles').hasClass('active')) {
+    $('.shop__cards').removeClass('active');
+    $('.candles').addClass('active');
+  };
+});
