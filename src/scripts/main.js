ce9c3121c7be21ff11484a8fdd685e64b6009e1e
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-30 17:12:54 +0300

    js for shop slider was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const buttonRight = document.querySelector('.shop__slider-button--right ');
+const buttonLeft = document.querySelector('.shop__slider-button--left');
+const shop = document.querySelectorAll('.shop__tabs .shop__galery');
+
+shop.forEach(item => (item.style.overflowX = 'hidden'));
+// document.querySelector('.shop__galery ').style.overflowX = 'hidden';
+
+buttonRight.addEventListener('click', event => {
+  shop.forEach(item => {
+    item.scrollLeft += document.querySelector('.shop__item--image ').width;
+  });
+});
+
+buttonLeft.addEventListener('click', event => {
+  shop.forEach(item => {
+    item.scrollLeft -= document.querySelector('.shop__item--image ').width;
+  });
+});
