ecaf0ea53d002c756f8fa660d6b83b5263ef7645
Andrii Hrushetskyi <41700032+Andrii256@users.noreply.github.com>
2020-09-13 13:47:34 +0300

    big update, finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function menuHidder() {
+  const checbox = document.querySelector('#menuToggler');
+  const linksList = document.querySelector('.header__ul');
+
+  linksList.onclick = () => {
+    /* menu showed only when neigbor checkbox checked */
+    checbox.checked = false;
+  };
+};
+
+menuHidder();
