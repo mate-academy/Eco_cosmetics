57c8271b17d63090d53fcaa195b686ebca8d00c9
KhrystynaYelyseyeva <play1409@gmail.com>
2020-09-19 00:32:21 +0300

    add section shop, aside slang

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const body = document.querySelector('.page');
 const burgerItem = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
-const closeItem = document.querySelector('.nav__close-burger');
+const closeItem = document.querySelector('.close-burger');
 
 burgerItem.addEventListener('click', () => {
   menu.classList.add('header__nav--active');
