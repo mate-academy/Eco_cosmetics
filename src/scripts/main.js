e985c110b495224e7768309ca826e8a5382531e9
Yurii Rysyn <zerocool_cs@ukr.net>
2020-09-20 12:16:43 +0300

    form submit and anchors were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,32 @@
 'use strict';
 
-const buttonRight = document.querySelector('.shop__slider-button--right ');
-const buttonLeft = document.querySelector('.shop__slider-button--left');
+const shopBtnRight = document.querySelector('.shop__slider-button--right ');
+const shopBtnLeft = document.querySelector('.shop__slider-button--left');
 const shop = document.querySelectorAll('.shop__tabs .shop__galery');
 
 shop.forEach(item => (item.style.overflowX = 'hidden'));
-// document.querySelector('.shop__galery ').style.overflowX = 'hidden';
 
-buttonRight.addEventListener('click', event => {
+shopBtnRight.addEventListener('click', event => {
   shop.forEach(item => {
     item.scrollLeft += document.querySelector('.shop__item--image ').width;
   });
 });
 
-buttonLeft.addEventListener('click', event => {
+shopBtnLeft.addEventListener('click', event => {
   shop.forEach(item => {
     item.scrollLeft -= document.querySelector('.shop__item--image ').width;
   });
+});
+
+const contactForm = document.querySelector('.contacts__form');
+
+contactForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  [...document.querySelectorAll('.contacts__form-input')].map(item => {
+    item.value = '';
+
+    return item;
+  });
+  document.querySelector('.contacts__form-textarea').value = '';
 });
