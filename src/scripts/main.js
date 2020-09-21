95f8883e1e84c44f96e7f71afac6e2e493fa7b47
Ivan Beletskiy <beletsky02@gmail.com>
2020-09-21 13:03:47 +0300

    burger-menu ready 100%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,22 @@
 'use strict';
 
-const body = document.querySelector('.page');
+const html = document.querySelector('html');
 const burgerItem = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
 const closeItem = document.querySelector('.close-burger');
+const navItem = document.querySelector('.nav__item');
 
 burgerItem.addEventListener('click', () => {
   menu.classList.add('header__nav--active');
-  body.style.overflow = 'hidden';
+  html.style.overflow = 'hidden';
 });
 
 closeItem.addEventListener('click', () => {
   menu.classList.remove('header__nav--active');
-  body.style.overflow = 'scroll';
+  html.style.overflow = 'scroll';
+});
+
+navItem.addEventListener('click', () => {
+  menu.classList.remove('header__nav--active');
+  html.style.overflow = 'scroll';
 });
