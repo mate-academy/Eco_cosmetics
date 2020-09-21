01f22b8f311c38ea5981bc194c4811f6234e2fa3
Ivan Beletskiy <beletsky02@gmail.com>
2020-09-21 12:38:30 +0300

    mobnav almost done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const body = document.querySelector('.page');
+const burgerItem = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const closeItem = document.querySelector('.close-burger');
+
+burgerItem.addEventListener('click', () => {
+  menu.classList.add('header__nav--active');
+  body.style.overflow = 'hidden';
+});
+
+closeItem.addEventListener('click', () => {
+  menu.classList.remove('header__nav--active');
+  body.style.overflow = 'scroll';
+});
