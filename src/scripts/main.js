5e154992f5c16f135924bf732c68540962b116cb
Andrey Gavriluk <bugabuga.008@gmail.com>
2020-11-29 18:04:18 +0200

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger');
+const burgerWrapper = document.querySelector('.nav__burger-wrapper');
+const navList = document.querySelector('.nav__list');
+
+burgerWrapper.addEventListener('click', function() {
+  burger.classList.toggle('nav__burger_close');
+  navList.classList.toggle('nav__list_opened');
+});
