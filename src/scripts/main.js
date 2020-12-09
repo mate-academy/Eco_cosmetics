d356934a1260c634714ccd37bfff1dab00ba8112
ShyianO <ol.shyian@gmacil.com>
2020-12-09 09:14:57 +0200

    Footer, burger menu, adaptation to medium screens were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navButton = document.querySelector('.header__burger-btn');
+const togglerButton = document.querySelector('.header__burger-toggler');
+const fixedBody = document.querySelector('body');
+
+navButton.addEventListener('click', () => {
+  togglerButton.classList.toggle('header__burger-toggler--active');
+  burgerMenu.classList.toggle('burger-menu--show');
+  fixedBody.classList.toggle('fixed');
+});
