7f064c6360d9cc2404b6451df8ad43e1c5ef8654
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-02-03 20:34:36 +0200

    three sections added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,1 @@
 'use strict';
-
-// керування елементами за допомогою js (керув дом.деревом)
-// let icon = document.querySelector(".shopping-bag");
-// let body = document.querySelector("body");
-
-// icon.addEventListener("click", function() {
-//   // console.log("hello");
-//   icon.style.background="#8e8d8c";
-// })
