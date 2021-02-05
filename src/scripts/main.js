a985b26adebef238bbf0b2bf1ccbd93fc75a1f9f
Artem Topolskyi <topolskyiav@gmail.com>
2021-02-05 14:14:51 +0200

    added burger menu and mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const navEl = document.querySelector('.burger-menu__navigation');
+const toggle = document.getElementById('toggle');
+
+function disableNav(event) {
+  if (event.target.tagName !== 'A') {
+    return;
+  }
+
+  toggle.checked = false;
+}
+
+navEl.addEventListener('click', disableNav);
