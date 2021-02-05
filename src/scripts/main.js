bbd7707a6c81481b61c9a81c2929698690449eaa
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-02-05 14:36:53 +0200

    shop block in process

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+$('.shop__slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  // variableWidth: true
+
+  nextArrow: '.shop__prev',
+  prevArrow: '.shop__next',
+});
+/* eslint-env jquery */
