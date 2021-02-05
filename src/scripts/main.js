d37565fcbc8d282b3708daf1d598c79f4a0570ed
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-02-05 19:08:52 +0200

    shop slick slider with filter added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,29 @@
 'use strict';
 
+$('.shop__nav-button').click(function() {
+  $('.shop__nav-button').removeClass('shop__nav-button--active');
+  $(this).addClass('shop__nav-button--active');
+});
+
 $('.shop__slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
   // variableWidth: true
 
   nextArrow: '.shop__prev',
   prevArrow: '.shop__next',
+});
+
+$('.slick-filter button').click(function() {
+  const dc = $(this).attr('id');
+  const filter = $('.slick-slider');
+
+  if (dc !== 'show_all') {
+    $('.slick-slider').slick('slickUnfilter');
+    filter.slick('slickFilter', '.' + dc);
+  } else {
+    $('.slick-slider').slick('slickUnfilter');
+  }
 });
 /* eslint-env jquery */
