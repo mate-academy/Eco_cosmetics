ff07546c31a189f9919b2e765fca6760ce736d08
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-02-06 16:38:00 +0200

    styles changed

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,6 +4,8 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileDropRef = document.querySelector('[data-lang]');
+  const moveContentDown = document.querySelector('.brand');
+  const buttonHamburger = document.querySelector('.header__hamburger');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded
@@ -15,4 +17,8 @@
     mobileMenuRef.classList.toggle('is-open');
     mobileDropRef.classList.toggle('is-open');
   });
+  buttonHamburger.addEventListener('click', (event) => {
+    moveContentDown.classList.toggle('brand--active-header');
+  });
+
 })();
