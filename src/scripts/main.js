4f591106e93726b0ec9a07e4587c155ed5ed4a06
Ivan Grekov <grekovv76@gmail.com>
2021-04-12 19:55:11 +0300

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const burgerToggler = document.querySelector('.burger-toggler');
+const nav = document.querySelector('.nav');
+const button = document.querySelector('#toggler');
+
+button.addEventListener('click', () => {
+  burgerToggler.classList.toggle('burger-toggler--active');
+  nav.classList.toggle('nav--active');
+});
+
+const newTitleText = 'Brand of<br>eco-cosmetics and magic candles';
+const mediaQuery = window.matchMedia('(min-width: 800px)');
+
+if (mediaQuery.matches) {
+  document.querySelector('.header__title').innerHTML = newTitleText;
+}
