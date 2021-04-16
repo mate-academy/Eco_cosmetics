3d1a18cb7325854cc014faecf3b5c9caad9186f6
Ivan Grekov <grekovv76@gmail.com>
2021-04-16 16:37:49 +0300

    switching sections of store was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,57 @@ const mediaQuery = window.matchMedia('(min-width: 800px)');
 if (mediaQuery.matches) {
   document.querySelector('.header__title').innerHTML = newTitleText;
 }
+
+// Changing store section
+
+// First section
+const faceLabel = document.querySelector('#store-sections-label-1');
+const faceSection = document.querySelector(
+  '.section-shop__grid-container--face'
+);
+
+// Second section
+const bodyLabel = document.querySelector('#store-sections-label-2');
+const bodySection = document.querySelector(
+  '.section-shop__grid-container--body'
+);
+
+// Third section
+const hairLabel = document.querySelector('#store-sections-label-3');
+const hairSection = document.querySelector(
+  '.section-shop__grid-container--hair'
+);
+
+// Fourth section
+const candleLabel = document.querySelector('#store-sections-label-4');
+const candleSection = document.querySelector(
+  '.section-shop__grid-container--candles'
+);
+
+faceLabel.addEventListener('click', () => {
+  faceSection.classList.add('section-shop__grid-container--active');
+  bodySection.classList.remove('section-shop__grid-container--active');
+  hairSection.classList.remove('section-shop__grid-container--active');
+  candleSection.classList.remove('section-shop__grid-container--active');
+});
+
+bodyLabel.addEventListener('click', () => {
+  faceSection.classList.remove('section-shop__grid-container--active');
+  bodySection.classList.add('section-shop__grid-container--active');
+  hairSection.classList.remove('section-shop__grid-container--active');
+  candleSection.classList.remove('section-shop__grid-container--active');
+});
+
+hairLabel.addEventListener('click', () => {
+  faceSection.classList.remove('section-shop__grid-container--active');
+  bodySection.classList.remove('section-shop__grid-container--active');
+  hairSection.classList.add('section-shop__grid-container--active');
+  candleSection.classList.remove('section-shop__grid-container--active');
+});
+
+candleLabel.addEventListener('click', () => {
+  faceSection.classList.remove('section-shop__grid-container--active');
+  bodySection.classList.remove('section-shop__grid-container--active');
+  hairSection.classList.remove('section-shop__grid-container--active');
+  candleSection.classList.add('section-shop__grid-container--active');
+});
