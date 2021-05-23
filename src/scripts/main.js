36558b008c48921941ebefe98bf4bd222847b5c9
Vlad Starzhitskiy <study.html.css.java@gmail.com>
2021-05-23 15:57:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const page = document.querySelector('.page');
+// const page = document.querySelector('.page');
 const toggler = document.querySelector('.header__menu-toggler');
 const menu = document.querySelector('.header__nav');
 
 const toggleMenu = () => {
   toggler.classList.toggle('header__menu-toggler--active');
   menu.classList.toggle('header__nav--active');
-  page.classList.toggle('page--fixed');
+  // page.classList.toggle('page--fixed');
 };
 
 toggler.addEventListener('click', toggleMenu);
