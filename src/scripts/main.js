d6a22fbd21f90a81878baf7ee5bab41eda800674
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-14 14:04:57 +0300

    target blank added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const nav = document.querySelector('.nav');
-const toggler = document.querySelector('.header__toggler');
-
-toggler.addEventListener('click', () => {
-  nav.classList.toggle('nav__nothidden');
-});
