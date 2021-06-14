149db438f062498d12078122831c2269d074bc49
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-14 13:46:41 +0300

    changes without burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const toggler = document.querySelector('.header__toggler');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav__nothidden');
+});
