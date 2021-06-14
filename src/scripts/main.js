aac694cd5735ef58713c84deaa4d3cf9025645c1
Tetiana Yasko <taniayasko2015@gmail.com>
2021-06-14 14:50:55 +0300

    final changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const toggler = document.querySelector('#toggler');
+const navLink = document.querySelectorAll('.nav__link');
+
+function removeToggler() {
+  for (const link of navLink) {
+    link.addEventListener('click', () => {
+      toggler.checked = false;
+    });
+  }
+}
+
+removeToggler();
