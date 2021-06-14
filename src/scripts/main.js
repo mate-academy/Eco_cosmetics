f02d4bcf01bbc8e01624e7fe8bbfc2442330cc34
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-06-14 14:45:12 +0300

    Added burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const burger = document.querySelector('.burger-menu');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileLink = document.querySelector('.mobile-nav__link');
+
+burger.addEventListener('click', () => {
+  mobileNav.classList.toggle('mobile-nav--active');
+  burger.classList.toggle('burger-menu--active');
+});
+
+mobileLink.addEventListener('click', () => {
+  mobileNav.classList.remove('mobile-nav--active');
+  burger.classList.remove('burger-menu--active');
+});
