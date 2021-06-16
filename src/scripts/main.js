d7ce2ba29285d6250e3a6ed3a726773c860e33d2
Sergiy Fartushniak <fartux97@gmail.com>
2021-06-16 20:46:46 +0300

    header with adaptive created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const mobileNav = document.querySelector('.mobile-nav');
+
+menu.addEventListener('click', () => {
+  mobileNav.classList.toggle('mobile-nav--active');
+  menu.classList.toggle('menu--active');
+});
