1e909f0490d21462cb6439dedaf86e6a864379d2
Sergiy Fartushniak <fartux97@gmail.com>
2021-06-21 22:57:20 +0300

    about us added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,14 @@
 
 const menu = document.querySelector('.menu');
 const mobileNav = document.querySelector('.mobile-nav');
+const mobileNavList = document.querySelector('.mobile-nav__list');
 
 menu.addEventListener('click', () => {
+  mobileNav.classList.toggle('mobile-nav--active');
+  menu.classList.toggle('menu--active');
+});
+
+mobileNavList.addEventListener('click', () => {
   mobileNav.classList.toggle('mobile-nav--active');
   menu.classList.toggle('menu--active');
 });
