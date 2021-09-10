75b6926bdade4c4c31eab1fa651c52075d8dfe31
Igor <i.kornienko1199@gmail.com>
2021-09-10 19:34:44 +0300

    slider done

--- src/app.js
+++ src/app.js
@@ -5,15 +5,27 @@ const next = document.querySelector('.sliders__right');
 const prev = document.querySelector('.sliders__left');
 const card = document.querySelector('.card');
 const cardWidth = card.offsetWidth;
+const getprop = window.getComputedStyle(list, null).getPropertyValue('gap');
+const prop = getprop.split('px');
+const numProp = +prop[0];
 
 let position = 0;
 
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+  next.disabled = position >= count - 3;
+  prev.disabled = position <= 0;
+  position = position % count;
+
+  list.style.transform = `translateX(${(-position * (cardWidth + numProp))}px)`;
+}
+
 next.addEventListener('click', () => {
-  position += 1;
-  list.style.transform = `translateX(${(-position * cardWidth)}px)`;
+  move(1);
 });
 
 prev.addEventListener('click', () => {
-  position -= 1;
-  list.style.transform = `translateX(${-position * cardWidth}px)`;
+  move(-1);
 });
