5a0c3fd1040ea8624a3049373ca7534ba4be0b38
Denis Demyanok <DenisRusis@ex.ua>
2021-09-11 15:15:08 +0300

    Eco cosmetics solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,29 @@ mobileNavList.addEventListener('click', () => {
   mobileNav.classList.toggle('mobile-nav--active');
   menu.classList.toggle('menu--active');
 });
+
+const list = document.querySelector('.shop__wrapper-list');
+const prev = document.querySelector('.shop__back');
+const next = document.querySelector('.shop__forvard');
+
+let position = 0;
+
+move(0);
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+
+  position = (position + count) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
+
+next.addEventListener('click', () => {
+  move(1);
+});
+
+prev.addEventListener('click', () => {
+  move(-1);
+});
