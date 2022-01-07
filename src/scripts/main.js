0851286da4f05ac9de9c88757f0d4c5de0cc601a
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-01-08 00:24:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // eslint-disable-next-line no-return-assign
+  inputs.forEach(elem => elem.value = '');
+});
