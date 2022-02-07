8e0e9bcdf5048504dd07151efb94c8a2f80cbb20
Mary <anokhinamasha27@gmail.com>
2022-02-07 09:01:29 +0200

    header, menu, footer - done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function openMenu() {
+  const menu = document.querySelector('.menu');
+  const btnOpenMenu = document.querySelector('.burger-menu');
+
+  function toggleMenu() {
+    document.body.classList.toggle('page--with-menu');
+    menu.classList.toggle('page__menu--open');
+  }
+
+  btnOpenMenu.addEventListener('click', toggleMenu);
+
+  menu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('menu__link')) {
+      toggleMenu();
+    } else if (event.target.classList.contains('menu__closer')) {
+      toggleMenu();
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  openMenu();
+});
