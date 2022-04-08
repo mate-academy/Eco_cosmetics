a7799fb924317616a4c2e3e633f70d3502061241
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-04-08 22:43:06 +0300

    fix

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -131,3 +131,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const icon = document.getElementById('header-burger');
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+
+icon.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item1.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item2.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item3.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
