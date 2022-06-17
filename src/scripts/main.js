16bb97fe972fcc4a4de88c34190bc4c52ee1f100
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-06-17 15:00:59 +0300

    card done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,22 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#Hydrophilic-oil') {
+    document.body.classList.add('page__body--with-card');
+  } else {
+    document.body.classList.remove('page__body--with-card');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#ubtan') {
+    document.body.classList.add('page__body--with-card');
+  } else {
+    document.body.classList.remove('page__body--with-card');
+  }
+});
+
 document.getElementById('form').addEventListener('click', function(event) {
   event.preventDefault();
 });
