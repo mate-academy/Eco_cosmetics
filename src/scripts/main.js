e930346469c44733b37801aa2af9974ac4b1d144
Yevhen Nikolaienko <illnino380@gmail.com>
2022-07-01 09:24:24 +0300

    added hovers, fixed textarea height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,14 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#shop') {
+    document.body.classList.add('page__body--with-shop');
+  } else {
+    document.body.classList.remove('page__body--with-shop');
+  }
+});
+
 const form = document.querySelector('.contact-us__form');
 
 form.addEventListener('submit', function(event) {
