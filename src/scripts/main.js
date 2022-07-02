27a07bf1152877144175c0ce2d3a5a26bcf555ff
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-07-02 11:42:19 +0300

    minor bags were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,15 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+window.addEventListener('hashchange', () => {
+  // eslint-disable-next-line
+  if (window.location.hash === '#to-shop--oil' || window.location.hash === '#to-shop--ubtan') {
+    document.body.classList.add('page--with-shop-card');
+  } else {
+    document.body.classList.remove('page--with-shop-card');
+  }
+});
+
 const form = document.querySelector('.form');
 
 // eslint-disable-next-line
