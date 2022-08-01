315ff1d690033bf13c0eb39c8346e0411d0edaa5
Cyril Prue <kir8mir>
2022-08-01 15:12:15 +0300

    product selected scroll added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.querySelectorAll('a.products__link').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const scrollTop = window.pageYOffset;
+    const href = this.getAttribute('href').replace('#', '');
+    const el = document.getElementById(href);
+
+    window.scrollTo({
+      top: el.getBoundingClientRect().top + scrollTop - 150, behavior: 'smooth',
+    });
+    el.parentNode.scrollLeft = el.offsetLeft - el.parentNode.offsetLeft;
+  });
+});
