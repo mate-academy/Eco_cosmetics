623a337fe95869632d5b1161f6ba5bc8eef24c0e
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-08-02 00:28:37 +0200

    Eco_cosmetics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+// document.getElementById('focus').focus();
+
+document.querySelector('.contact-us__form').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+    document.querySelector('.contact-us__form').reset();
+  });
