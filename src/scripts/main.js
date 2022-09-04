ebc6c1fb481710152cbb52a897e1eee4ff41991d
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-09-04 10:08:15 +0300

    menu scrolling fixed, added hover effect for benefits, added instagram and facebook links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu');
+  } else {
+    document.body.classList.remove('menu');
+  }
+});
