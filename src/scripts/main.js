93e017e6b3c53f548cbac62ade8b70d758497b8e
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-06 12:35:37 +0300

    header + brand - mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+    nav.classList.add('nav--visible');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+    nav.classList.remove('nav--visible');
+  }
+});
