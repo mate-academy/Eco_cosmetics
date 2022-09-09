e2a4cbcb737b6151310b43b29aec8ce4d8f8c251
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-09 16:11:12 +0300

    work is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,4 +20,47 @@ document.querySelector('.form').addEventListener('submit', (event) => {
   for (const input of formInputs) {
     input.value = '';
   }
+
+  window.scrollBy(-10000, -10000);
 }, false);
+
+document.querySelector('input[name="tel"]')
+  .addEventListener('input', event => {
+    event.target.value = event.target.value
+      .split('').filter(c => !isNaN(+c)).join('');
+  });
+
+const buttonsBox = document.querySelector('.shop__buttons');
+const field = document.querySelector('.shop__card-slider');
+
+const buttons = buttonsBox.children;
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    moveTo(i);
+  });
+}
+
+function moveTo(pos) {
+  hightlight(pos);
+
+  if (window.screen.width < 768) {
+    field.style.transform = `translateX(${-pos * 100}%)`;
+  } else if (window.screen.width < 1280) {
+    field.style.transform = `translateX(calc(${-pos * 50}% - ${pos * 15}px))`;
+  } else {
+    field.style.transform = `translateX(calc(${-pos * 50}% - ${pos * 10}px))`;
+  }
+}
+
+function hightlight(newPosition) {
+  const activeDot = buttonsBox.querySelector('.shop__item--active');
+
+  if (activeDot) {
+    activeDot.classList.remove('shop__item--active');
+  }
+
+  const selected = buttonsBox.children[newPosition];
+
+  selected.classList.add('shop__item--active');
+}
