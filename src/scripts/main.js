ed73dc7107d9dc9baa836d79fcdc45ae94b164e5
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-09-18 22:30:20 +0300

    newUpdate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,38 +8,6 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#shipping') {
-    document.body.classList.add('page_body-with-shipping');
-  } else {
-    document.body.classList.remove('page_body-with-shipping');
-  }
-});
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#products') {
-    document.body.classList.add('page_body-with-products');
-  } else {
-    document.body.classList.remove('page_body-with-products');
-  }
-});
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#payment') {
-    document.body.classList.add('page_body-with-payment');
-  } else {
-    document.body.classList.remove('page_body-with-payment');
-  }
-});
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#confirmation') {
-    document.body.classList.add('page_body-with-confirmation');
-  } else {
-    document.body.classList.remove('page_body-with-confirmation');
-  }
-});
-
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', event => {
