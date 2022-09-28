0ebcaf368b9233bc920c2c3a577d6691421e1fbe
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-09-28 21:48:41 +0300

    Landing done v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#add') {
+    document.body.classList.add('page__body--with-menu');
+  } else if (window.location.hash === '#shipping') {
+    document.body.classList.add('page__body--with-menu');
+  } else if (window.location.hash === '#payment') {
+    document.body.classList.add('page__body--with-menu');
+  } else if (window.location.hash === '#confirmation') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contacts__main-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.reset();
+});
+
+const formTwo = document.querySelector('.shipping__form');
+
+formTwo.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  formTwo.reset();
+});
