fae415999078ce92935fe33b63505815e0b40f78
Bogdan Pasokha <MyPikcha@gmail.com>
2022-10-06 19:33:46 +0300

    eco cosmetics landing was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
 'use strict';
+document.querySelector('.contact-us__form').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+    document.querySelector('.contact-us__form').reset();
+  })
