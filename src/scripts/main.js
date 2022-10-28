3a6b3fccc765199b7a1a59d289a987cc989598ab
Denis Bokov <denisbokov1703@gmail.com>
2022-10-28 15:15:09 +0200

    Eco cosmetics (5)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
+
+
 const forms = document.querySelectorAll('.form__field');
 
 const formField = document.querySelector('.form, .form__shipping');
