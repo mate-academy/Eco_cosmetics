c6a062dd3cff55fb73a9eaae4fc18a99d41b488d
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-10-28 11:16:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field');
+const textarea = document.querySelector('.form-field--textarea');
+// const dropdownButton = document.querySelector('.dropdown__button');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+
+  textarea.value = '';
+});
+
+const products = ['#hydrophilic-oil', '#face-cream', '#ubtan'];
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+
+  if (products.includes(window.location.hash)) {
+    document.body.classList.add('page__body--with-product');
+  } else {
+    document.body.classList.remove('page__body--with-product');
+  }
+});
+
+const tabs = document.querySelector('.shop__tabs');
+const shopTabs = document.querySelectorAll('.shop__tab');
+
+tabs.addEventListener('click', (event) => {
+  shopTabs.forEach((tab) => {
+    if (tab === event.target.closest('li')) {
+      tab.classList.add('shop__tab--active');
+    } else {
+      tab.classList.remove('shop__tab--active');
+    }
+  });
+});
