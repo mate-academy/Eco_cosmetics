67f64d3c2c7308464f751e91593497e169914177
Kocherha Roman <f.your.d.2017@gmail.com>
2023-01-20 13:45:16 +0200

    all bags fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,8 +8,9 @@ form.addEventListener('submit', (event) => {
 });
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu' 
-    || window.location.hash === '#shop-card') {
+  if (window.location.hash === '#menu'
+    || window.location.hash === '#shop-card'
+    || window.location.hash === '#shipping') {
     document.body.classList.add('page__body--with--menu');
   } else {
     document.body.classList.remove('page__body--with--menu');
