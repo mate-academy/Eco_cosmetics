5ca29529d09ca7f7ee236f944c69102e614f0809
Alina Isakova <adorosh062@gmail.com>
2023-01-22 18:57:10 +0200

    eco

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const form = document.querySelector('.contacts__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if ((window.location.hash === '#menu')
+  || (window.location.hash === '#open-card')
+  || (window.location.hash === '#menu-card')
+  || (window.location.hash === '#open-card2')) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
