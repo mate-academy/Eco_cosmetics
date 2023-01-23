6e7b21323494e690786be5902c46ffa241027c01
Kocherha Roman <f.your.d.2017@gmail.com>
2023-01-23 22:08:45 +0200

    fixes bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,11 @@ form.addEventListener('submit', (event) => {
 });
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu'
+  if (
+    window.location.hash === '#menu'
     || window.location.hash === '#shop-card'
-    || window.location.hash === '#shipping') {
+    || window.location.hash === '#shipping'
+  ) {
     document.body.classList.add('page__body--with--menu');
   } else {
     document.body.classList.remove('page__body--with--menu');
