544a80695fd03bfbe0faf64fd7e944070d0f9ab1
Polina Kotyrlo <plinakotyrlo@gmail.com>
2023-02-17 13:04:17 +0200

    edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,5 +4,13 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  event.target.removeEventListener();
+  event.target.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
