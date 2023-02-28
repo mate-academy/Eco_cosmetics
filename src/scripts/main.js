ca034e13cd7f63367c8a7fdffe27c39ac65ced11
Natalia Matash <natalimatash@gmail.com>
2023-02-28 15:08:23 +0200

    Ctrated Menus section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menus') {
+    document.body.classList.add('page__body--with-menus');
+  } else {
+    document.body.classList.remove('page__body--with-menus');
+  }
+});
