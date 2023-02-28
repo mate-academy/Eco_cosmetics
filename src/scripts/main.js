a10d1d2b074e60aedfd7b2a6cdb5cd75de52357c
Іван Вигінний <ivan665303@gmail.com>
2023-02-28 22:58:56 +0200

    Fix first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,32 @@
 'use strict';
 
-// HAMBURGER MENU
+// LOADING
+const spinner = document.querySelector('.loader');
 
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.menu__list');
+window.onload = function() {
+  spinner.style.display = 'block';
+};
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-
-  document.body.style.overflowY = (
-    document.body.style.overflowY === 'hidden'
-      ? 'visible' : 'hidden'
-  );
+window.addEventListener('load', () => {
+  spinner.style.display = 'none';
 });
 
-document.querySelectorAll('.menu__link').forEach(link => (
-  link.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  }))
-);
+// HAMBURGER MENU
+
+const hamburger = document.querySelectorAll('.hamburger');
+const navMenu = document.querySelectorAll('.menu__list');
+
+hamburger.forEach(item => (
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+    navMenu.forEach(nav => (nav.classList.toggle('active')));
+
+    document.body.style.overflowY = (
+      document.body.style.overflowY === 'hidden'
+        ? 'visible' : 'hidden'
+    );
+  })
+));
 
 // SWITCHER BETWEEN SHOP ITEMS
 
@@ -43,7 +49,7 @@ links.forEach((link) => {
   });
 });
 
-// FORM PREVENT RESET
+// PREVENT FORM RESET
 
 const form = document.querySelector('.contact-us__form');
 
@@ -55,24 +61,81 @@ form.addEventListener('submit', (e) => {
 // BUY AND ADD TO BAG
 
 const itemImage = document.querySelectorAll('.shop__item-img');
-const closeShopItem = document.querySelector('.item__cross');
+const closeShopItem = document.querySelectorAll('.item__cross');
 const shopItem = document.querySelector('.shop__item');
+const shipping = document.querySelector('.shipping');
 const itemButton = document.querySelector('.item__button');
+const addtoCart = document.querySelector('.shipping__button');
+const payment = document.querySelector('.payment');
+const paymentButton = document.querySelector('.payment__button');
+const confirmation = document.querySelector('.confirmation');
+const confirmationLink = document.querySelector('.confirmation__link');
 
-const appendItem = () => {
-  shopItem.classList.add('show');
+function appendItem(item) {
+  item.classList.add('show');
   document.body.classList.add('stop-scrolling');
   document.body.classList.add('overlay');
 };
 
-const closeItem = () => {
-  shopItem.classList.remove('show');
+function closeItem(item) {
+  item.classList.remove('show');
+
   document.body.classList.remove('stop-scrolling');
   document.body.classList.remove('overlay');
 };
 
-itemImage.forEach(item => item.addEventListener('click', appendItem));
+itemImage.forEach(item => item.addEventListener('click',
+  () => appendItem(shopItem)));
 
-closeShopItem.addEventListener('click', closeItem);
+closeShopItem.forEach(cross => (
+  cross.addEventListener('click',
+    () => {
+      closeItem(shopItem);
+      closeItem(shipping);
+      closeItem(payment);
+    }
+  )
+));
 
-itemButton.addEventListener('click', closeItem);
+confirmationLink.addEventListener('click', () => {
+  closeItem(confirmation);
+  closeItem(shopItem);
+  closeItem(shipping);
+  closeItem(payment);
+});
+
+itemButton.addEventListener('click', () => appendItem(shipping));
+
+addtoCart.addEventListener('click', () => appendItem(payment));
+
+paymentButton.addEventListener('click', () => appendItem(confirmation));
+
+// SHIPPING FORM VALIDATION
+
+const shippingForm = document.querySelector('.shipping__form');
+const inputs = shippingForm.querySelectorAll('shipping__input');
+const submitButton = shippingForm.querySelector('button');
+
+function validateInputs() {
+  let valid = true;
+
+  inputs.forEach(input => {
+    if (!input.value) {
+      valid = false;
+    }
+  });
+
+  return valid;
+}
+
+shippingForm.addEventListener('input', () => {
+  if (validateInputs()) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
+
+shippingForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
