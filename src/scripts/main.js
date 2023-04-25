87e19344bd7ad4f54dbad306f9e6e0ac5d164bd9
Semchuk Rodion <semchukrodion@gmail.com>
2023-04-25 18:59:41 +0300

    create cosmetics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if ((window.location.hash === '#menu')
+  || (window.location.hash === '#card')) {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contact__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
