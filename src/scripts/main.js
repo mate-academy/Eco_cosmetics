897975ac65df8eee5a8f4505a3e8beb2164a56cb
Ruslan Liapin <joy.esenin@gmail.com>
2023-04-29 13:08:08 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ formReload.addEventListener('submit', (e) => {
   e.preventDefault();
   formReload.reset();
 });
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  if (link.getAttribute('href').startsWith('http')) {
+    link.setAttribute('target', '_blank');
+  }
+});
