7e68e14bb0aaf3d0395e342145f74a264db6ec04
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-05-22 21:19:35 +0100

    form is not submitted on empty fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,23 +7,3 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
-const inputs = document.querySelectorAll('input');
-
-const button = document.getElementById('button-send');
-
-button.addEventListener('click', (ev) => {
-  let shouldPreventDefault = false;
-
-  for (const input of inputs) {
-    if (input.value === '') {
-      shouldPreventDefault = true;
-    } else {
-      shouldPreventDefault = false;
-    }
-
-    if (shouldPreventDefault) {
-      ev.preventDefault();
-    }
-  }
-});
