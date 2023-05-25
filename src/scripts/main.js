16be255c85f617626fd6bca004ffb9560c8e21c4
Vova Zinchenko <zincenkomain17@gmail.com>
2023-05-25 14:48:41 +0300

    fix sumbit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,29 +24,13 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('form');
+document.querySelector('#consult').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  form.reset();
-});
-
-const textarea = document.querySelector('textarea');
-const limitCols = 35;
-const limitRows = 3;
-
-textarea.addEventListener('keyup', (e) => {
-  const lines = e.target.value.split('\n');
-
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].length < limitCols) {
-      continue;
-    }
-
-    lines[i] = lines[i].substring(0, limitCols);
-    lines[i + 1] = `${lines[i].substring(limitCols + 1)}${lines[i + 1] || ''}`;
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('tel').value = '';
   }
-
-  textarea.value = lines.slice(0, limitRows).join('\n');
-});
+);
