a84314055369f942b05dd1d35fec2f4873a52836
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-06-01 18:45:05 +0300

    added hover effects for social links , fixed contact us image , removed scroll bar from menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,8 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu'
-    || window.location.hash === '#basket1') {
+    || window.location.hash === '#basket1'
+    || window.location.hash === '#basket2') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
