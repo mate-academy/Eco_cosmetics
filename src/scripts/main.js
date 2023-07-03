fa4858a453f0acb4b4a019567cb545d1b914615d
Kostia Hudzenko <kostya.gudzenko1991@gmail.com>
2023-07-03 14:11:45 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,18 @@ window.addEventListener('hashchange', () => {
     : document.body.classList.remove('page__body--with-menu');
 });
 
+window.addEventListener('hashchange', () => {
+  window.location.hash === '#hydrophilic-oil'
+    ? document.body.classList.add('page__body--with-oil')
+    : document.body.classList.remove('page__body--with-oil');
+});
+
+window.addEventListener('hashchange', () => {
+  window.location.hash === '#ubtan'
+    ? document.body.classList.add('page__body--with-ubtan')
+    : document.body.classList.remove('page__body--with-ubtan');
+});
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   e.target.reset();
