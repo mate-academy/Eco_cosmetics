19ee12d2b7070c9194448929d1260ef9f6305cb4
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-07-08 14:09:40 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#cart-1' && '#cart-2') {
+    document.body.classList.add('page__body--with-cart');
+  } else {
+    document.body.classList.remove('page__body--with-cart');
+  }
+});
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
