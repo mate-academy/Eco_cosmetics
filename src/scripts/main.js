ab7049d11c955b443e00ab18e47845facf286693
Mykhailiuk Viktor <117285070+Viktor535x@users.noreply.github.com>
2023-08-15 11:48:06 +0300

    test10

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const form = document.querySelector('#coact-us');
-
-function submit(event) {
-  event.preventDefault();
-  form.reset();
-}
-
-form.addEventListener('submit', submit);
-
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#shopF') {
+    document.body.classList.add('page_shopF--with-menu');
+  } else {
+    document.body.classList.remove('page_shopF--with-menu');
   }
 });
