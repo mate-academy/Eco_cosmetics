b337c178b19f7c2048717802503b12c1cefad674
Mykhailiuk Viktor <117285070+Viktor535x@users.noreply.github.com>
2023-08-15 20:49:12 +0300

    test11

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,9 @@
 'use strict';
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#menu' || window.location.hash === '#shopF') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
-  }
-});
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#shopF') {
-    document.body.classList.add('page_shopF--with-menu');
-  } else {
-    document.body.classList.remove('page_shopF--with-menu');
   }
 });
