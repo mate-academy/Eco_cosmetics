5975e7dfa26fa4f45193393c358b4416ed29ff07
Wita Shchurko <vitaliya.shchurko@gmail.com>
2023-09-03 12:56:16 +0300

    create Eco cosmetics landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const div = document.querySelector('.tab__img');
+const height = document.querySelector(':root');
+
+calcWidth();
+window.addEventListener('resize', calcWidth);
+
+function calcWidth() {
+  height.style.setProperty('--height', div.clientWidth + 'px');
+}
