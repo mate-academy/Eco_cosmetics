26b6f8ea8d55d00ac19415511b12e59a92741c71
AndruhaMan <andreysut12@gmail.com>
2023-09-04 21:28:04 +0300

    The task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#hydrophilic-oil') {
+    document.body.classList.add('page__body--with-oil');
+  } else {
+    document.body.classList.remove('page__body--with-oil');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#ubtan') {
+    document.body.classList.add('page__body--with-ubtan');
+  } else {
+    document.body.classList.remove('page__body--with-ubtan');
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
