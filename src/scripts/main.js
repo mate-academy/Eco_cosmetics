2cb59201cbd570f8aa1d199a089df36572538c6e
Kirill <ovoo1339@gmail.com>
2023-10-08 12:14:05 +0300

    fix2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,13 +14,77 @@ function scrollAuthor() {
   targetElement.scrollIntoView({ behavior: 'smooth' });
 }
 
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+  const form = document.querySelector('.contact__form');
+  const inputElements = document.querySelectorAll('.contact__input');
+
+  const name = form.querySelector('[placeholder="Your Name"]').value;
+  const email = form.querySelector('[placeholder="E-Mail"]').value;
+  const phone = form.querySelector('[placeholder="Phone Number"]').value;
+  const message = form.querySelector('[placeholder="Your Message"]').value;
+
+  // eslint-disable-next-line no-undef
+  fetch('https://mate.academy/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name, email, phone, message,
+    }),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      // eslint-disable-next-line no-console
+      console.log('Данные успешно отправлены', data);
+
+      inputElements.forEach(function(inputElement) {
+        inputElement.classList.remove('completed', 'error');
+        inputElement.value = '';
+      });
+
+      form.reset();
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('Ошибка при отправке данных:', error);
+
+      inputElements.forEach(function(inputElement) {
+        inputElement.classList.remove('completed', 'error');
+        inputElement.value = '';
+      });
+    });
+}
+
+function validateInput(inputElement) {
+  const value = inputElement.value.trim();
+  const type = inputElement.type;
+
+  if (type === 'email') {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return emailRegex.test(value);
+  } else if (type === 'tel') {
+    return value.length >= 6;
+  } else {
+    return value.length >= 6;
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const inputElements = document.querySelectorAll('.contact__input');
 
   inputElements.forEach(function(inputElement) {
     inputElement.addEventListener('blur', function() {
       if (this.value.trim() !== '') {
-        // eslint-disable-next-line no-undef
         if (validateInput(this)) {
           this.classList.add('completed');
           this.classList.remove('error');
@@ -29,47 +93,12 @@ document.addEventListener('DOMContentLoaded', function() {
           this.classList.remove('completed');
         }
       } else {
-        this.classList.remove('completed');
-        this.classList.remove('error');
+        this.classList.remove('completed', 'error');
       }
     });
   });
 
   const form = document.querySelector('.contact__form');
 
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = form.querySelector('[placeholder="Your Name"]').value;
-    const email = form.querySelector('[placeholder="E-Mail"]').value;
-    const phone = form.querySelector('[placeholder="Phone Number"]').value;
-    const message = form.querySelector('[placeholder="Your Message"]').value;
-
-    // Очистить состояния ошибок у всех полей ввода
-    inputElements.forEach(function(inputElement) {
-      inputElement.classList.remove('completed', 'error');
-    });
-
-    // eslint-disable-next-line no-undef
-    fetch('https://mate.academy/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name, email, phone, message,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        // eslint-disable-next-line no-console
-        console.log('Данные успешно отправлены', data);
-        form.reset();
-      })
-      .catch(error => {
-        // eslint-disable-next-line no-console
-        console.error('Ошибка при отправке данных:', error);
-        form.reset();
-      });
-  });
+  form.addEventListener('submit', handleFormSubmission);
 });
