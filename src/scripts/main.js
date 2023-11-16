d6fc43d9dcf07753652ba5ee71e61270f2cc9057
Serhii Babskyi <91350215+SerhiiBabskyi@users.noreply.github.com>
2023-11-16 20:33:18 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__bodu--with-menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
