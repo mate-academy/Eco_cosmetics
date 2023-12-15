c0d3fc7dc2e8dc8ae24398ef3c27620629c09145
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2023-12-15 13:00:55 +0200

    add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const tabs = document.querySelectorAll('.shop__tab');
+const allContent = document.querySelectorAll('.shop__product-carts');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(tab => {
+      tab.classList.remove('shop__tab--active');
+    });
+    tab.classList.add('shop__tab--active');
+
+    allContent.forEach(content => {
+      content.classList.remove('shop__product-carts--active');
+    });
+    allContent[index].classList.add('shop__product-carts--active');
+  });
+});
+
+$(() => {
+  $('.slider').slick();
+});
