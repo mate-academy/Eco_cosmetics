a39117cbe95fa57abb5c8cd9bc0f14ea677d556a
LukashevychSergey <lukashevychsergey@gmail.com>
2024-01-29 19:51:22 +0200

    solved issues

--- src/scripts/product.js
+++ src/scripts/product.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// prevent sending the form
-
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  form.reset();
-});
-
-// hiding the scroll when menu opens
-
 const openMenuButton = document.getElementById('openMenu');
 const closeMenuButton = document.getElementById('closeMenu');
 const navItems = document.querySelectorAll('.nav__link');
