851df8012cd6ac1534d7dbefe9754e4e4a2b47e2
Oksana Tytanych <oksanatytanych@gmail.com>
2024-02-08 13:33:19 +0200

    Eco cosmetics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+/* <<<--- add active class to links in the shop section --->>> */
+
+const shopLinks = document.querySelectorAll('.shop__link');
+
+function activeLink(event) {
+  event.stopPropagation();
+
+  const currentLink = document.querySelector('.active');
+
+  if (currentLink) {
+    currentLink.classList.remove('active');
+  }
+
+  this.classList.toggle('active');
+}
+
+shopLinks.forEach(item => item.addEventListener('click', activeLink));
+
+/* <<<--- show all products in the shop section --->>> */
+
+const allProductsButton = document.querySelector('#all-products');
+const cards = document.querySelectorAll('.card');
+
+function allVisible(event) {
+  event.stopPropagation();
+  cards.forEach(card => card.classList.add('card-visible'));
+}
+
+if (allProductsButton) {
+  allProductsButton.addEventListener('click', allVisible);
+}
