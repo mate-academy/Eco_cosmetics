a99f3f1ef3a575ec32f4eefb8848d98b5579fa9a
Bohdan Bilozor <boturt@gmail.com>
2024-03-28 18:43:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuIcon = document.querySelector('.nav__icon.menu');
+  const aside = document.querySelector('.aside');
+  const closeIcon = document.querySelector('.aside__icon-close');
+  const links = document.querySelectorAll('.aside__link');
+
+  function toggleScroll() {
+    aside.classList.toggle('open');
+    document.body.classList.toggle('no-scroll');
+  }
+
+  menuIcon.addEventListener('click', toggleScroll);
+  closeIcon.addEventListener('click', toggleScroll);
+
+  links.forEach(link => {
+    link.addEventListener('click', toggleScroll);
+  });
+});
