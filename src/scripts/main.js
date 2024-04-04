1a3df372ac813827e24cac88ffa8189720717220
Serhii Khobotov <hobbitura@gmail.com>
2024-04-04 20:16:16 +0300

    Demo link added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const addedContent1 = document.querySelector('.about-brand__added-content');
-const addedText1 = addedContent1.querySelector('.about-brand__added-text')
-const learnMore1 = addedContent1.querySelector('.about-brand__learn-more');
+const addedText1 = addedContent1.querySelector('.about-brand__added-text');
+const learnMore1 = addedContent1.querySelector('.about-brand__learnMore');
 
 learnMore1.addEventListener('click', () => {
   addedText1.classList.toggle('about-brand__added-text--active');
-})
+});
 
 const addedContent2 = document.querySelector('.about-creators__added-content');
-const addedText2 = addedContent2.querySelector('.about-creators__added-text')
-const learnMore2 = addedContent2.querySelector('.about-creators__learn-more');
+const addedText2 = addedContent2.querySelector('.about-creators__added-text');
+const learnMore2 = addedContent2.querySelector('.about-creators__learnMore');
 
 learnMore2.addEventListener('click', () => {
   addedText2.classList.toggle('about-creators__added-text--active');
-})
+});
 
 const addedContent3 = document.querySelector('.tooth-powder__added-content');
-const addedText3 = addedContent3.querySelector('.tooth-powder__added-text')
-const learnMore3 = addedContent3.querySelector('.tooth-powder__learn-more');
+const addedText3 = addedContent3.querySelector('.tooth-powder__added-text');
+const learnMore3 = addedContent3.querySelector('.tooth-powder__learnMore');
 
 learnMore3.addEventListener('click', () => {
   addedText3.classList.toggle('tooth-powder__added-text--active');
-})
+});
