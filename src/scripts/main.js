8539249e892ce115bf74f52a114e4a9f98f3a219
Mykyta Kalniev <nikitamal4ik@gmail.com>
2024-11-05 16:31:09 +0100

    mobile version v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var form = document.getElementById("form");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+document.querySelector('form').onsubmit = e => {
+  e.target.reset();
+  return false;
+};
