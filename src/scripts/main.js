bf8dafcadd83ded2c3ef052be5ac3d2cee350f25
renata-aline <renatalinec@gmail.com>
2024-12-06 17:05:37 -0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('visible');
+}
+
+const links = document.querySelectorAll('.shop__tab_list_link-item');
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function () {
+
+    for (let item = 0; item < links.length; item++) {
+      links[i].classList.remove('active');
+    }
+
+    this.classList.add('active');
+  });
+}
