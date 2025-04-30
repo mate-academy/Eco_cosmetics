a062aa268140a3f864cd001d635c7c72aa5122a7
Marcin Maka <marcinmaka37@gmail.com>
2025-04-30 08:16:03 +0200

    Setting slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,4 +21,30 @@ function openShop(evt, shopName) {
   evt.currentTarget.className += " active";
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("tabcontent");
+  let dots = document.getElementsByClassName("tablinks");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
