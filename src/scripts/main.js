551db1bc7d458d5633b2aefa48913bbb72bacc4a
Paulina <paulina.zwolenik@gmail.com>
2025-09-14 19:38:00 +0200

    fix code to requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,35 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-product');
   }
 });
+
+const categoryLinks = document.querySelectorAll('.shop__link');
+const categoryBlocks = document.querySelectorAll('.shop__category');
+
+categoryLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    const category = link.getAttribute('data-category');
+
+    e.preventDefault();
+
+    if (category) {
+      categoryBlocks.forEach(block => {
+        block.classList.remove('active');
+      });
+
+      const target = document.querySelector(`.shop__category[data-category="${category}"]`);
+      if (target) {
+        target.classList.add('active');
+      }
+    }
+  });
+});
+
+const clickableImages = document.querySelectorAll('.shop__image-first, .shop__image-second');
+
+clickableImages.forEach(img => {
+  img.addEventListener('click', () => {
+    if (window.matchMedia('(max-width: 1279px)').matches) {
+      window.location.href = '#product';
+    }
+  });
+});
