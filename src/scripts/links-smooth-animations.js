b255defd60cd50ea2833a1f1f438e94c0456b7a6
bagirovoleg <o.bagirov.pl@gmail.com>
2025-09-24 10:52:42 +0200

    feat: Add link animations and working navigation

---
+++ src/scripts/links-smooth-animations.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href').slice(1);
+    const target = document.getElementById(targetId);
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
